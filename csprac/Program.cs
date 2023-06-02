4f34a9d2beaa839e65bbde8757da37ac4a679e74
Luan <97277916+lnngn@users.noreply.github.com>
2023-06-02 10:16:37 -0700

    init c# on win

--- csprac/Program.cs
+++ csprac/Program.cs
@@ -2,5 +2,6 @@
 string name = Console.ReadLine();
 Console.WriteLine("My name is " + name);
 Console.WriteLine("Great! Hello " + name + ", Welcome to the jungle");
+Console.WriteLine("This is where you go hard!!!!!!");
 
 
