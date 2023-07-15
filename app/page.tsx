8e06d2ac4279bddb7cbf868ed4bb98a65c1fa49f
Luan <97277916+hluan-dev@users.noreply.github.com>
2023-07-15 16:34:00 -0700

    todo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onSetText = (e: any) => {
+    setText(e.target.value);
+  };
+
+  const onSetToDos = () => {
+    const newToDos = [...toDos];
+  };
+
   return (
     <main className="">
-      <div>This is kinda cool</div>
-      <p>Wow this is pretty cool</p>
-      <p>I wonder this could be the greatest idea ever</p>
+      <form onSubmit={onSetToDos}>
+        <input
+          type="text"
+          value={text}
+          onInput={onSetText}
+          className="border border-black"
+        />
+        <button type="submit">Add</button>
+      </form>
+
+      {toDos.map((toDo, index) => (
+        <p key={index}>{toDo}</p>
+      ))}
     </main>
   );
 }
