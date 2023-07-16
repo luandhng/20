9428780b8a4ff345c76ae942698a1454ccbeda9e
Luan <97277916+hluan-dev@users.noreply.github.com>
2023-07-16 00:39:54 -0700

    init tauri

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,7 @@
-"use client";
-
-import { useState } from "react";
-
 export default function Home() {
-  const [text, setText] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onSetText = (e: any) => {
-    setText(e.target.value);
-  };
-
-  const onSetToDos = () => {
-    const newToDos = [...toDos];
-  };
-
   return (
     <main className="">
-      <form onSubmit={onSetToDos}>
-        <input
-          type="text"
-          value={text}
-          onInput={onSetText}
-          className="border border-black"
-        />
-        <button type="submit">Add</button>
-      </form>
-
-      {toDos.map((toDo, index) => (
-        <p key={index}>{toDo}</p>
-      ))}
+      <h1>Love</h1>
     </main>
   );
 }
