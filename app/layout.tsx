d5e331fcb143fb2682be52ea43dffa3a0b82fa6e
Luan <97277916+luan-n@users.noreply.github.com>
2023-10-07 10:27:32 -0700

    updated

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Supertime",
+  description: "Manage your organizations in an easy way",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="">
+      <h1>Supertime</h1>
+      <p>Manage your organizations in an easy way</p>
+    </main>
+  );
+}
