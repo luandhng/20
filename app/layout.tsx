4f7a55cd0d4e2a03d0e6349434d78063e10c5446
luan <97277916+hluandev@users.noreply.github.com>
2023-12-25 16:48:27 -0800

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "The Japanese Project",
+  title: "Supertime",
   description: "",
 };
 
