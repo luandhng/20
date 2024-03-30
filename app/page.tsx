04d7f2adb345fd8a37b66a94e385b052d6fc461b
hluandev <97277916+hluandev@users.noreply.github.com>
2024-03-30 00:58:00 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import { Menu } from "@/components/menu";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="relative max-w-2xl mx-auto py-52 leading-loose">
+      <Menu />
+      <p>
+        Much of the art I appreciate in museums can be bucketed into aesthetics
+        like typographic, medieval, or patterned. The boundaries feel clearly
+        defined because we can assign names to them. I have been increasingly
+        more stimulated by aesthetic intersections—unexpected displays of art
+        that tastefully reject the notion of a clean, singularly defined style,
+        yet bridges many. For my taste, often this ends up in opposition to
+        minimalism, yet does not chaotically lean into kitschy or maximalism.
+        There is authoring intent, purpose, and a sense of iteration felt
+        throughout, not seemingly arbitrary excess for the sake of creative
+        provocation. I believe there's something fascinating in art and design
+        that treats my attention with care and instills curiosity.
+      </p>
+    </main>
+  );
 }
---
+++ components/menu.tsx
@@ -0,0 +1,7 @@
+export const Menu = () => {
+  return (
+    <div className="absolute -left-56">
+      <h1 className="">0. Install</h1>
+    </div>
+  );
+};
