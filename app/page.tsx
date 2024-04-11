29aeb1a38ab881de755b18c7ee0a7c6bb394d6b3
Luan <97277916+hluandev@users.noreply.github.com>
2024-04-10 22:36:11 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="text-green-400">
       <h1>Wow you are coolff</h1>
-      <p>Hello</p>
+      <p>Helleweo</p>
     </main>
   );
 }
